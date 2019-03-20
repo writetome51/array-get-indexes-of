fbbae7f8d435bb98f8486e067d0b87ea8899d05a
Steve Thompson <writetome51@gmail.com>
2019-03-20 16:31:54 -0600

    Refactored implementation code for getLastIndexOf(). Upped version to 1.0.1

--- dist/privy/getLastIndexOf.js
+++ dist/privy/getLastIndexOf.js
@@ -1,14 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var isEmpty_notEmpty_1 = require("basic-data-handling/isEmpty_notEmpty");
-var getIndexesOf_1 = require("./getIndexesOf");
+var array_get_copy_1 = require("@writetome51/array-get-copy");
+var getFirstIndexOf_1 = require("./getFirstIndexOf");
 // returns -1 if value being searched for isn't found.
-// value cannot be object.
+// value cannot be object.  It can be an array.
 function getLastIndexOf(value, array) {
-    var indexes = getIndexesOf_1.getIndexesOf(value, array);
-    if (isEmpty_notEmpty_1.isEmpty(indexes))
-        return -1;
+    var reversedCopy = array_get_copy_1.getCopy(array).reverse();
+    var i = getFirstIndexOf_1.getFirstIndexOf(value, reversedCopy);
+    if (i === -1)
+        return i;
     else
-        return indexes[indexes.length - 1];
+        return (reversedCopy.length - 1 - i);
 }
 exports.getLastIndexOf = getLastIndexOf;
--- lib/privy/getLastIndexOf.ts
+++ lib/privy/getLastIndexOf.ts
@@ -1,11 +1,12 @@
-import { isEmpty } from 'basic-data-handling/isEmpty_notEmpty';
-import { getIndexesOf } from './getIndexesOf';
+import { getCopy } from '@writetome51/array-get-copy';
+import { getFirstIndexOf } from './getFirstIndexOf';
 
 // returns -1 if value being searched for isn't found.
-// value cannot be object.
+// value cannot be object.  It can be an array.
 
 export function getLastIndexOf(value, array): number {
-	let indexes = getIndexesOf(value, array);
-	if (isEmpty(indexes)) return -1;
-	else return indexes[indexes.length - 1];
+	let reversedCopy = getCopy(array).reverse();
+	let i = getFirstIndexOf(value, reversedCopy);
+	if (i === -1) return i;
+	else return (reversedCopy.length - 1 - i);
 }
